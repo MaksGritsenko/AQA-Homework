0649309495eb0fe808be6c2b7d366264eea548dd
h.maksym <gremmaks@gmail.com>
2024-05-29 19:00:32 +0300

    - add homework nine

--- src/main/java/homeworknine/Counter.java
+++ src/main/java/homeworknine/Counter.java
@@ -3,18 +3,18 @@ package homeworknine;
 public class Counter {
 
     private String name;
-    private int count;
+    private int occurrence;
 
     public Counter (String s , int i) {
         this.name = s;
-        this.count = i;
+        this.occurrence = i;
     }
 
     @Override
     public String toString() {
         return "{" +
                 "name:'" + name + '\'' +
-                ", count:" + count +
+                ", count:" + occurrence +
                 '}';
     }
 }
